f77058679c18baec15b5e890fe5d59de4b9cd4f7
Arayik <mailtoharutyunyan@gmail.com>
2022-01-08 02:40:48 +0400

    test

---
+++ src/main/java/am/test/teamcity/SpirngTeamcityApplication.java
@@ -0,0 +1,13 @@
+package am.test.teamcity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpirngTeamcityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpirngTeamcityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/am/test/teamcity/SpirngTeamcityApplicationTests.java
@@ -0,0 +1,13 @@
+package am.test.teamcity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpirngTeamcityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
